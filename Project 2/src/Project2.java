7cc9d63a4d0d9b4b00d33eba118d7f677c8034aa
JustinWong28 <justin.wong2000@comcast.net>
2017-08-22 09:28:26 -0700

    second Project

---
+++ Project 2/src/Project2.java
@@ -0,0 +1,15 @@
+
+// Justin Wong
+// My first java project. It will compare print and println
+// August 22, 2017
+
+public class Project2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub 
+	System.out.println("hello this is Justin Wong's project 2!!!");
+	
+	System.out.print("hello this is Justin Wong's project 2!!!");
+	}
+
+}
