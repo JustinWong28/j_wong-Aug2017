ff93f00091d69d2ec5d002f179af28ec4f71b52e
JustinWong28 <justin.wong2000@comcast.net>
2017-09-05 09:29:14 -0700

    calculator

---
+++ Access Calculate/src/DoMath.java
@@ -0,0 +1,15 @@
+/* Justin Wong
+ * September 5, 2017
+ * This is runner class fir my calculate library methods
+ */
+ 
+ public class DoMath {
+
+	public static void main(String[] args) {
+		Calculate.square(5);
+System.out.println(Calculate.square(5));
+System.out.println(Calculate.cubed(3));
+System.out.println(Calculate.average(20,25));
+System.out.println(Calculate.average2(20,40,30);
+	}
+}
---
+++ Calculate library/src/Calculate.java
@@ -0,0 +1,29 @@
+/* Justin Wong
+ September 5,2017
+ This is library that will contain various mathematical methods.
+ */
+public class Calculate {
+	/*
+	 * This method takes an integer and returns it square.
+	 */
+	public static int square(int operand) {
+		return (operand * operand);
+		/*
+		 * this method takes an integer and returns its cube
+		 */
+	}
+
+	public static int cubed (int operand) {
+		return (operand * operand *operand);
+	}
+
+	/*
+	 * this method takes two doubles and returns one double public static double
+	 */
+	  average (double operand1, double operand2 ) { return ((operand1+
+	  operand2)/2); } /*this method does the same as average but returns with three
+	  doubles
+	 
+public static double average2 (double operand1, operand2, operand3) {
+return ((operand1+ operand2+ operand3)/3);
+}
