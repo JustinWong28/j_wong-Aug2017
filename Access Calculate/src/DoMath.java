935e42e2b2182e89bc766bcff7e704a526214fac
JustinWong28 <justin.wong2000@comcast.net>
2017-09-06 11:31:16 -0700

    new changes

--- Access Calculate/src/DoMath.java
+++ Access Calculate/src/DoMath.java
@@ -9,7 +9,12 @@
 		Calculate.square(5);
 System.out.println(Calculate.square(5));
 System.out.println(Calculate.cubed(3));
-System.out.println(Calculate.average(20,25));
+System.out.println(Calculate.average(20,40));
 System.out.println(Calculate.average2(20,40,30));
+System.out.println(Calculate.toDegrees(20));
+System.out.println(Calculate.toRadians(3.14159));
+System.out.println(Calculate.Discriminant(1, 2, 3));
+System.out.println(Calculate.toImproperFrac(3, 1, 2));
+System.out.println(Calculate.toMixedNum(7 , 2));
 	}
 }
--- Calculate library/src/Calculate.java
+++ Calculate library/src/Calculate.java
@@ -20,13 +20,45 @@ public class Calculate {
 	/*
 	 * this method takes two doubles and returns one double public static double
 	 */
-	double average (double operand1, double operand2 ) { 
-		return (( operand1+ operand2) / 2); 
+	public static double average(double operand1, double operand2) {
+		return ((operand1 + operand2) / 2);
 	}
 
 	/*
 	 * this method does the same as average but returns with three doubles
 	 */
-	public static double average2 (double operand1, double operand2,double operand3) {
-		return ((operand1+ operand2+ operand3)/3);
+	public static double average2 (double operand1 , double operand2 , double operand3) {
+		double average= (( operand1 + operand2 + operand3)/3);
+		return average;
 	}
+	
+	/*
+	 *  This methood accepts a double and returns a double
+	 */
+	public static double toDegrees (double degrees) {
+	return degrees*(3.14159/180);
+	}
+/* this method converts an anglle measure givenin degrees into radians
+ */
+	
+public static double toRadians (double radians) {
+	return radians*(180/3.1459);
+}
+ 
+/* This method accepts three doubles and returns a double
+ */
+public static double Discriminant (double a , double b , double c) {
+	return ((b*b)-(4*a*c));
+}
+/* this method converts mixed number into an improper fraction
+ */
+public static String toImproperFrac (int a , int b, int c) {
+	return (a*c+b +"/" + c);
+}
+/* this method converts an improper fraction into a mixed number
+ */
+public static String toMixedNum (int a , int b) {
+	return (a/b) + " " + (a%b + "/" +b);
+}
+
+}
