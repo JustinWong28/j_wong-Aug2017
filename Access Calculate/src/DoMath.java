da473b77df0647a04c9fb5997732ceea9be08ac5
JustinWong28 <justin.wong2000@comcast.net>
2017-09-26 09:25:10 -0700

    part 4

--- Access Calculate/src/DoMath.java
+++ Access Calculate/src/DoMath.java
@@ -5,19 +5,31 @@
  
  public class DoMath {
 
-	public static void main(String[] args) {
-		Calculate.square(5);
-System.out.println(Calculate.square(5));
-System.out.println(Calculate.cubed(3));
-System.out.println(Calculate.average(20,40));
-System.out.println(Calculate.average2(20,40,30));
-System.out.println(Calculate.toDegrees(20));
-System.out.println(Calculate.toRadians(3.14159));
-System.out.println(Calculate.Discriminant(1, 2, 3));
-System.out.println(Calculate.toImproperFrac(3, 1, 2));
-System.out.println(Calculate.toMixedNum(7 , 2));
-System.out.println(Calculate.foil(2, 3, 6, -7));
-System.out.println(Calculate.max(3, 1, 1));
-System.out.println(Calculate.round2(-33.494));
+		public static void main(String[] args) {
+			System.out.println(Calculate.square(5));
+			System.out.println(Calculate.cube(5));
+			System.out.println(Calculate.average(5, 7));
+			System.out.println(Calculate.average(5,7,9));
+			System.out.println(Calculate.toDegrees(2*3.14149));
+			System.out.println(Calculate.toRadians(360));
+			System.out.println(Calculate.discriminant(1,2,3));
+			System.out.println(Calculate.toImproperFrac(3,1,2));
+			System.out.println(Calculate.toMixedNumber(7,2));
+			System.out.println(Calculate.foil(2,3,6, -7));
+			System.out.println(Calculate.isDivisibleBy(4,2));
+			System.out.println(Calculate.absValue(-20));
+			System.out.println(Calculate.max(1, 2));
+			System.out.println(Calculate.max(1,2,3));
+			System.out.println(Calculate.min(1,2));
+			System.out.println(Calculate.round2(3.526));
+			System.out.println(Calculate.exponent(4,2));
+			System.out.println(Calculate.factorial(5));
+			System.out.println(Calculate.isPrime(7));
+			System.out.println(Calculate.gcf(36, 12));
+			System.out.println(Calculate.sqrt(-100));
+
+		}
+
 	}
-}
+
+
--- Calculate library/src/Calculate.java
+++ Calculate library/src/Calculate.java
@@ -3,125 +3,271 @@
  This is library that will contain various mathematical methods.
  */
 public class Calculate {
+
 	/*
-	 * This method takes an integer and returns it square.
+	 * This method takes an integer and returns its square.
+	 * 
 	 */
 	public static int square(int operand) {
-		return (operand * operand);
-		/*
-		 * this method takes an integer and returns its cube
-		 */
-	}
-
-	public static int cubed(int operand) {
-		return (operand * operand * operand);
+		int squared = operand * operand;
+		return squared;
 	}
 
 	/*
-	 * this method takes two doubles and returns one double public static double
+	 * This method takes and integer and returns its cube
+	 * 
+	 */
+	public static int cube(int operand) {
+		int cubed = operand * operand * operand;
+		return cubed;
+	}
+
+	/*
+	 * This method accepts two doubles and returns the average as a double
+	 * 
 	 */
 	public static double average(double operand1, double operand2) {
-		return ((operand1 + operand2) / 2);
+		double mean = (operand1 + operand2) / 2;
+		return mean;
 	}
 
 	/*
-	 * this method does the same as average but returns with three doubles
+	 * This method does the same as average but with three doubles
+	 * 
 	 */
-	public static double average2 (double operand1 , double operand2 , double operand3) {
-		double average= (( operand1 + operand2 + operand3)/3);
-		return average;
+	public static double average(double operand1, double operand2, double operand3) {
+		double mean = (operand1 + operand2 + operand3) / 3;
+		return mean;
 	}
 
 	/*
-	 *  This methood accepts a double and his returns a double
+	 * This method accepts a double and returns a double Converts an angle in
+	 * radians into a measure in degrees.
 	 */
-	public static double toDegrees (double degrees) {
-		return degrees*(3.14159/180);
-	}
-	/* this method converts an anglle measure givenin degrees into radians
-	 */
-
-	public static double toRadians (double radians) {
-		return radians*(180/3.1459);
+	public static double toDegrees(double radians) {
+		double degrees = radians * 180 / 3.14159;
+		return degrees;
 	}
 
-	/* This method accepts three doubles and returns a double
+	/*
+	 * This method converts an angle measure from degrees to radians. Accepts a
+	 * double and returns a double
 	 */
-	public static double Discriminant (double a , double b , double c) {
-		return ((b*b)-(4*a*c));
+	public static double toRadians(double degrees) {
+		double radians = degrees * 3.14159 / 180;
+		return radians;
 	}
-	/* this method converts mixed number into an improper fraction
+
+	/*
+	 * This method will provide coefficients of a quadratic equation in standard
+	 * form (a,b,c) and returns the value of the discriminant
 	 */
-	public static String toImproperFrac (int a , int b, int c) {
-		return (a*c+b +"/" + c);
+	public static double discriminant(double a, double b, double c) {
+		double discriminant = b * b - 4 * a * c;
+		return discriminant;
 	}
-	/* this method converts an improper fraction into a mixed number
+
+	/*
+	 * This method will convert mixed numbers into an improper fraction from three
+	 * integers to a string
 	 */
-	public static String toMixedNum (int a , int b) {
-		return (a/b) + " " + (a%b + "/" +b);
+	public static String toImproperFrac(int a, int b, int c) {
+		return a * c + b + "/" + c;
 	}
-	/* this method acepts a four integers and a string and then returns a string
+
+	/*
+	 * Converts improper fraction into a mixed number two integers to a string
 	 */
-	public static String foil ( int a , int  b , int c , int d) {
-		return ((a*c) + "x^2" + "+" + ((c-a) + "x") + "+" + (b*d));
+	public static String toMixedNumber(int a, int b) {
+		return a / b + " " + a % b + "/" + b;
+
 	}
-	/* this method accepts two integers and returns a boolean
-	 */ 
-	public static boolean isDivisibleBy (int a , int b) {
-		if (a%b==0) {
+
+	/*
+	 * Converts (ax + b)(cx + d) into a quadratic equation of ax^2 + bx +c The
+	 * method accepts four integers and returns a string
+	 */
+	public static String foil(int a, int b, int c, int d) {
+		return a * c + "x^2" + "+" + (a * d + b * c) + "x" + b * d;
+	}
+
+	/*
+	 * determines whether or not one integer is evenly divisible by another accepts
+	 * two integers and returns a boolean
+	 */
+	public static boolean isDivisibleBy(int a, int b) {
+		if (b==0) {
+			throw new IllegalArgumentException();
+		}
+		if (a % b == 0) {
 			return true;
-		}else {
+		} else {
 			return false;
 		}
 	}
-	public static double absValue (double a) {
-		if (a>0) {
+
+	/*
+	 * This method will return the absolute value of the number passed. accepts a
+	 * double and returns a double
+	 */
+	public static double absValue(double a) {
+		if (a > 0) {
 			return a;
-		}
-		else if (a<0) {
+		} else if (a == 0) {
+			return 0.;
+		} else {// why can't I use else if
 			return -a;
 		}
-		return a;
-	} 
-	public static double max (double a, double b) {
-		if (a>b) {
+	}
+
+	/*
+	 * This method returns the larger of the values passed accepts 2 double and
+	 * returns a double
+	 */
+	public static double max(double a, double b) {
+		if (a > b) {
+			return a;
+		} else {// why can't i use else if
+			return b;
+		} // what if a==b
+	}
+
+	/*
+	 * Overload max accepts three doubles and returns a double
+	 */
+	public static double max(double a, double b, double c) {
+		if (a > b && a > c) {
+			return a;
+		} else if (b > a && b > c) {
+			return b;
+		} else if (c > a && c > b) {
+			return c;
+		} else {
 			return a;
 		}
-		else if (a<b) {
+	}
+
+	/*
+	 * Minimum finds minimum of two ints
+	 */
+	public static int min(int a, int b) {
+		if (a < b) {
+			return a;
+		} else if (b < a) {
 			return b;
+		} else {
+			return a;
 		}
-		return a;
 	}
-public static double max (double a, double b, double c) {
-	if (a>b && a>c) {
-		return a;
+
+	/*
+	 * This method will round a double to two decimal places returns a double
+	 */
+	public static double round2(double a) {
+		double b = (a * 1000) % 10;
+		if (b <= 4) {
+			return (a * 1000 - b) / 1000;
+		} else {
+			return (a * 1000 - b + 10) / 1000;
+		}
+
 	}
-	else if (a<b && b>c) {
-		return b;
+
+	/*
+	 * This method raises a to the positive integer value accepts a double and an
+	 * integer, returns a double assume exponent is positive
+	 */
+	public static double exponent(double a, int b) {
+		if (b==0) {
+			return 1;
+		}
+		if (b<0) {
+			throw new IllegalArgumentException();
+		}
+		double result = a;
+		for (int x = 2; x <= b; x++) {
+			result *= a;
+		}
+		return result;
 	}
-	else if (c>b && c>a) {
-		return c;
+
+	/*
+	 * This method returns the factorial of the value passed ! accepts and integer
+	 * and returns an integer
+	 */
+	public static int factorial(int a) {
+		if (a<0)
+			throw new IllegalArgumentException();
+		int answer = a;
+		for (int n = a; n >= 2; n--) {
+			answer *= n - 1;
+		}
+		return answer;
 	}
-	else if (a==b && a==c)
-		return a;
-	return a; 
-} 
-public static int min (int a, int b) {
-	if (a<b) {
-		return a;
+
+	/*
+	 * This method determines whether or not an integer is prime accepts an integer
+	 * and returns a boolean
+	 */
+	public static boolean isPrime(int number) {
+		/*
+		 * Need a variable to count number of variables name it factors count starts at
+		 * 1 because has at least itself
+		 */
+		int factorCount = 1;
+		for (int i = 2; i < number; i++) {
+			// i=2 because factor of 1 already works for every number
+			boolean test = isDivisibleBy(number, i);
+			if (test == true) {
+				factorCount++;// counts # of factors
+			}
+		}
+		if (factorCount > 1) {
+			return false;
+		} else {
+			return true;
+		}
 	}
-	else if (a>b) {
-		return b;
+
+	/*
+	 * This method will find the greatest common factor accepts two ints, returns
+	 * gcf in an int form
+	 */
+	public static int gcf(int greaterN, int lesserN) {
+		int A = greaterN;
+		int B = lesserN;
+		int result;
+		while (B != 0) {
+			result = A % B;
+			A = B;
+			B = result;
+		}
+		return A;
 	}
-	return a;
+
+	/*
+	 * This method returns an approximation of the square root accepts a double
+	 * returns a double
+	 */
+	public static double sqrt(double n) {
+		// how will I get the square root? loop?
+		// square root of N is about= ½(N/A + A)
+		// N is number to square root and then A is educated guess
+		// repeatedly replace educated guess
+		// return root
+		if (n<0) {
+			throw new IllegalArgumentException();
+		}
+		double root = 0;
+		double A = n / 2;
+		while (root != (n / A + A) / 2) {
+			root = (n / A + A) / 2;
+			A = root;
+		}
+		double answer = round2(root);
+		return answer;
+	}
+	
 }
-public static double round2 (double a) {
-	if (a*1000 % 10 <5) {
-		return (a * 1000 -a * 1000 % 10) / 1000;
-	}else {
-		return (10-a * 1000 % 10 + a * 1000)/ 1000;
-	}
-}
-public static double exponent {
-	for
-}
+
+
