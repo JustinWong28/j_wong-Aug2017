63265ac7029a7e8e007cefeefd7b78c9dc326440
JustinWong28 <justin.wong2000@comcast.net>
2017-10-26 08:46:33 -0700

    Split

--- Split/src/Split.java
+++ Split/src/Split.java
@@ -1,12 +1,15 @@
 
+import java.util.*;
 public class Split {
 
 	public static void main(String[] args) {
-
+		
 		//String.split();
 		//It's a method that acts on a string, <StringName>.split(<String sp>);
 		//Where sp is the string where the string splits
 		//And it returns an array
+		System.out.println(Arrays.toString("I like apples!".split(" ")));
+		System.out.println(Arrays.toString("I really like really red apples".split("really")));
 		// Example: "I like apples!".split(" "); 
 		//			it will split at spaces and return an array of ["I","like","apples!"]
 		// Example 2: "I really like really red apples".split("really")
