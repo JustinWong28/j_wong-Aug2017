f6a1664b95e938db20fb797bde8d3af5c8a9b196
JustinWong28 <justin.wong2000@comcast.net>
2017-10-27 08:54:22 -0700

    part 1

--- Split/src/Split.java
+++ Split/src/Split.java
@@ -10,6 +10,7 @@ public class Split {
 		//And it returns an array
 		System.out.println(Arrays.toString("I like apples!".split(" ")));
 		System.out.println(Arrays.toString("I really like really red apples".split("really")));
+		System.out.println(Arrays.toString("I reallyreally like apples".split("really")));
 		// Example: "I like apples!".split(" "); 
 		//			it will split at spaces and return an array of ["I","like","apples!"]
 		// Example 2: "I really like really red apples".split("really")
@@ -30,9 +31,14 @@ public class Split {
 		 * use String.split to split up the sandwich at the spaces, " ", and return what's in the middle of the sandwich and ignores what's on the outside
 		 * Again, what if it's a fancy sandwich with multiple pieces of bread?
 		 */
-
-
-
+	}
+public static String[] splitSandwich (String sandwich) {
+	String[] components= "applespineapplesbreadlettustomatobaconmayohambreadcheese".split("bread");
+	
+	return components;
+	
+	
+}
+	
 	}
 
-}
