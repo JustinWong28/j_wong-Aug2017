ba8b94d1f1cd86b5bc560fea14af8119f101ceb1
Justin <Justin@192.168.0.19>
2017-12-12 17:04:48 -0800

    FracCalc objects

---
+++ FracCalcObj/src/Fraction.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+
+public class Fraction {
+	private int whole;
+	private int numerator;
+	private int denominator;
+	private String operator;
+			
+	public void parseFraction(String operand) {
+		whole = 0;
+		numerator = 0;
+		denominator = 1;
+		int underscore = operand.indexOf("_");
+		int slash = operand.indexOf("/");
+		if (operand.indexOf("_") >= 0) {
+			
+			whole = Integer.parseInt(operand.substring(0 , underscore));
+			numerator = Integer.parseInt(operand.substring(underscore + 1, slash));
+			denominator = Integer.parseInt(operand.substring(slash + 1, operand.length()));
+		}else if ((operand.indexOf("_") < 0) && (operand.indexOf("/") >= 0)) {
+			
+			whole = 0;
+			numerator = Integer.parseInt(operand.substring(underscore + 1, slash));
+			denominator = Integer.parseInt(operand.substring(slash + 1, operand.length()));
+		}else if ((operand.indexOf("_") < 0) && (operand.indexOf("/") < 0)) {
+			
+			whole = Integer.parseInt(operand);
+			numerator = 0;
+			denominator = 1;
+		}
+	}
+	public static void toMixedNum() {
+
+	}
+	public void addSubtract() {
+		
+	}
+
+	public void multiplyDivide() {
+		
+	}
+	public int toInt(String str) {
+		
+	}
+	public void simplfy() {
+	
+		}
+	public String toString( ) {
+		
+	}
+}
