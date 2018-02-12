1ba36cad51e8efe5cfcaab63ef6a80d742a80905
JustinWong28 <justin.wong2000@comcast.net>
2018-02-12 08:53:22 -0800

    hi

---
+++ Prisms/Prism.java
@@ -0,0 +1,37 @@
+/* Write this abstract class called Prism.  It has one private field (height) and a constructor.  
+ * It contains the methods calcAreaOfBase, calcPerimeter, calcVolume, and calcSA.
+ * None of these methods require parameters.   
+ * A couple are abstract - can you tell which ones and why?
+ * 
+ * Math note:
+ * The volume of any prism can be found by multiplying the area of the base by the height.
+ * The surface area of any prism can be found by multiplying the perimeter of the base by the height
+ *   and then adding on the areas of the 2 bases.
+ */
+
+
+public class Prism  
+{
+	private double height;
+	
+	public Prism(double height) {
+		height = this.height;
+		}
+	
+	public double getHeight() {
+		return height;
+	}
+	public abstract double calcAreaOfBase();
+	public abstract double calcPerimeter();
+	public double calcVolume() {
+		double volume = height* calcAreaOfBase();
+		return volume;
+		}
+	public int calcSA() {
+		return (calcPerimeter() * height) + (2 * calcAreaOfBase());
+		
+	}
+	
+	
+}
+	
