d695393dfd6d2ddeb02b48b3ec6055eb04608a5e
JustinWong28 <justin.wong2000@comcast.net>
2018-05-23 11:31:41 -0700

    efficiency

--- TextExcel/src/textExcel/Spreadsheet.java
+++ TextExcel/src/textExcel/Spreadsheet.java
@@ -8,13 +8,10 @@ public class Spreadsheet implements Grid {
 	private int numCol = 12;
 	private Cell [][] grid= new Cell [20][12];
 	public Spreadsheet() {
-		for (int r= 0; r < 20; r++) {
-			for(int c = 0; c < 12; c++) {
-				grid[r][c] = new EmptyCell();
-			}
-		}
+		clear();
 	}
-	
+
+
 	public String processCommand(String command) {
 		String[] parsedCommand = command.split(" ", 3);			//split at the spaces, into 3 elements
 		if(parsedCommand.length == 2 && parsedCommand[0].toLowerCase().equals("clear")) {		//if length is 2 ("clear A1"), clear cell
@@ -25,12 +22,12 @@ public class Spreadsheet implements Grid {
 			return getGridText();
 		}else{
 			if(parsedCommand.length == 1 && parsedCommand[0].toLowerCase().equals("clear")) {	//if length is 1 and equals ("clear"), clear cell
-		clear();
-		return getGridText();
-			}else if(parsedCommand.length==1&&!parsedCommand[0].toLowerCase().equals("clear")){     			//cell inspection (e.g., A1). This should return the value at that cell
-			SpreadsheetLocation loc = new SpreadsheetLocation(parsedCommand[0]);
-			return getCell(loc).fullCellText();
-		}
+				clear();
+				return getGridText();
+			}else if(parsedCommand.length==1 && !parsedCommand[0].toLowerCase().equals("clear")){     			//cell inspection (e.g., A1). This should return the value at that cell
+				SpreadsheetLocation loc = new SpreadsheetLocation(parsedCommand[0]);
+				return getCell(loc).fullCellText();
+			}
 		}
 		return "";
 	}
@@ -47,7 +44,7 @@ public class Spreadsheet implements Grid {
 
 	@Override
 	public Cell getCell(Location loc) {
-		
+
 		return grid[loc.getRow()][loc.getCol()];
 	}
 
@@ -67,18 +64,18 @@ public class Spreadsheet implements Grid {
 				for(int j = 0; j < 12; j++) {
 					rowNumbers += grid[i][j].abbreviatedCellText();
 					rowNumbers += "|";
-					}
-					rowNumbers += "\n";
-				}else {
-					rowNumbers += (i+1);
-					rowNumbers += " |";
-					for(int j = 0; j < 12; j++) {
-						rowNumbers += grid[i][j].abbreviatedCellText();
-						rowNumbers += "|";
-					}
-					rowNumbers += "\n";
 				}
+				rowNumbers += "\n";
+			}else {
+				rowNumbers += (i+1);
+				rowNumbers += " |";
+				for(int j = 0; j < 12; j++) {
+					rowNumbers += grid[i][j].abbreviatedCellText();
+					rowNumbers += "|";
+				}
+				rowNumbers += "\n";
 			}
+		}
 		return colLetter + rowNumbers;
 
 	}
@@ -87,12 +84,12 @@ public class Spreadsheet implements Grid {
 		SpreadsheetLocation loc = new SpreadsheetLocation(cell);
 		return getCell(loc).fullCellText();
 	}
-	
+
 	//assigns value to given
 	public String assignValue(String cell, String input) {
 		SpreadsheetLocation loc = new SpreadsheetLocation(cell);
 		if(input.substring(0, 1).equals("\"")) {
-		grid[loc.getRow()][loc.getCol()] = new TextCell(input);
+			grid[loc.getRow()][loc.getCol()] = new TextCell(input);
 		}
 		else if(input.substring(input.length()-1).equals("%")) {
 			grid[loc.getRow()][loc.getCol()] = new PercentCell(input);
@@ -103,19 +100,19 @@ public class Spreadsheet implements Grid {
 		else {
 			grid[loc.getRow()][loc.getCol()] = new ValueCell(input);
 		}
-			return getGridText();
+		return getGridText();
 	}
-	
+
 	//clears the entire sheet
 	public String clear() {
 		for (int i = 0; i < 20; i++) {
 			for (int j = 0; j < 12; j++) {
-			grid[i][j] = new EmptyCell();
+				grid[i][j] = new EmptyCell();
 			}
 		}
 		return getGridText();
 	}
-	
+
 	//clears one cell
 	public void clearCell(String cell) {
 		SpreadsheetLocation loc = new SpreadsheetLocation(cell);
